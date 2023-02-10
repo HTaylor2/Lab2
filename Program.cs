565ac13d5e181857dee9b8c5853ba0b7fcf05e64
HTaylor2 <124090615+HTaylor2@users.noreply.github.com>
2023-02-09 20:57:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//harold taylor CPS 3330_01 Lab2
+using static System.Console;
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decison;
+            double numOne, numTwo, numThree;
+            
+            Console.WriteLine("Are you averaging two or three numbers?");
+            decison = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Enter the first number");
+                    numOne= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                    numTwo= Convert.ToInt32(Console.ReadLine());
+           
+            if(decison == 3 )
+            {
+                Console.WriteLine("Enter the third number");
+                numThree = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"The Average is {Average.GetAverage(numOne, numTwo, numThree)}");
+            }
+            else
+            {
+                Console.WriteLine($"The Average is {Average.GetAverage(numOne, numTwo)}");
+            }
+        }
+    }
+}
